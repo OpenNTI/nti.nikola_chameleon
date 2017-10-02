096320cf87660e681e13b1bfee7977caad3500d3
Jason Madden <jamadden@gmail.com>
2017-10-02 16:09:34 -0500

    A basic strategy is in place. Now to work on the base template.

--- README.rst
+++ README.rst
@@ -28,16 +28,33 @@ A template system for the `Nikola
 
 Nikola uses a `custom mechanism <https://pypi.python.org/pypi/yapsy>`_
 to find plugins instead of using the usual ``pkg_resources`` system.
-That makes it difficult to install plugins; it's not enough just to
-``pip install`` a package from PyPI. Instead, you must *also* copy a
-``.plugin`` file to a particular location on disk. This can be:
+That makes it *incredibly* difficult to install plugins; it's not enough
+just to ``pip install`` a package from PyPI. Instead, you must *also*
+copy a ``.plugin`` file to a particular location on disk. This can be:
 
 - ~/.nikola/plugins/
 - The ``plugins`` directory of your Nikola site.
+
+Beside that '.plugin' file there must also be a '.py' file of the same
+name that the plugin lists as a module (yes, even though the plugin file
+specifically requests a Python module, yapsy requires that it be a
+file or directory beside the plugin file---so not really a module).
+
+It's ridiculous to require everyone to copy plugins into their plugin
+folder (they're not even correctly on sys.path, meaning that
+zope.configuration and many other tools won't work) and we don't plan
+to let Nikola do that automatically (we're not on the Nikola plugin
+index and won't be until they let us do standard installs), so the
+best we can do is attempt to workaround yapsy's limitations.
+
+Into your site's plugin directory, place the following .py file::
+
+  # nti_nikola_chameleon.py
+  from nti.nikola_chameleon import *
+
 
 ..
   Rendering the plugin file must be last, it is done by
   setup.py (since PyPI does not support the ..include directive.)
 
-The contents of a correct ``nti.nikola_chameleon.plugin`` file are
-delivered with this package. They are also rendered here::
+Beside that, you'll need a ``nti.nikola_chameleon.plugin`` file::
--- src/nti/nikola_chameleon/plugin.py
+++ src/nti/nikola_chameleon/plugin.py
@@ -3,23 +3,30 @@
 The Chameleon ZPT plugin for nikola.
 
 """
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+from __future__ import absolute_import, division, print_function
 
-# stdlib imports
+import glob
 import os
 import os.path
 
 from chameleon.zpt.template import PageTemplateFile
 from nikola.plugin_categories import TemplateSystem
 from nikola.utils import makedirs
+
+import nti.nikola_chameleon
 import z3c.pt.pagetemplate
+from z3c.macro.interfaces import IMacroTemplate
+from z3c.macro.zcml import MacroFactory
+import z3c.macro.zcml
+from zope import component
+from zope import interface
+from zope.configuration import xmlconfig
 from zope.dottedname import resolve as dottedname
+
 
 logger = __import__('logging').getLogger(__name__)
 
-class _PageTemplateFileWithLoad(z3c.pt.pagetemplate.PageTemplateFile):
+class _ViewPageTemplateFileWithLoad(z3c.pt.pagetemplate.ViewPageTemplateFile):
     """
     Enables the load: expression type for convenience.
     """
@@ -32,17 +39,34 @@ class _PageTemplateFileWithLoad(z3c.pt.pagetemplate.PageTemplateFile):
 
     @property
     def builtins(self):
-        d = super(_PageTemplateFileWithLoad, self).builtins
+        d = super(_ViewPageTemplateFileWithLoad, self).builtins
         d['__loader'] = self._loader
         return d
 
 z3c.pt.pagetemplate.BaseTemplate.expression_types['load'] = PageTemplateFile.expression_types['load']
+z3c.macro.zcml.ViewPageTemplateFile = _ViewPageTemplateFileWithLoad
+z3c.macro.zcml.ViewPageTemplateFile = _ViewPageTemplateFileWithLoad
 
+class ITemplate(interface.Interface):
+    """
+    A template.
+    """
+
+class TemplateFactory(object):
+
+    def __init__(self, path):
+        self.path = path
+
+    def __call__(self, *_ignored):
+        return _ViewPageTemplateFileWithLoad(self.path)
 
 class ChameleonTemplates(TemplateSystem):
 
     name = 'nti.nikola_chameleon'
 
+    def __init__(self):
+        super(ChameleonTemplates, self).__init__()
+        xmlconfig.file('configure.zcml', nti.nikola_chameleon)
 
     def set_directories(self, directories, cache_folder):
         """Sets the list of folders where templates are located and cache."""
@@ -50,8 +74,14 @@ class ChameleonTemplates(TemplateSystem):
         # located. Most template systems have some sort of
         # template loading tool that can use this.
 
-        # Does nothing, chameleon doesn't have the direct concept of a path,
-        # only relative to the file with the load: expression.
+        # But we can get macros and etc from the directory.
+        # XXX What about entire templates? We need a way to deal
+        # with those. Zope would use a <page> or <view> directive.
+        # The Chamelean PageTemplateFile supports a 'search_path' argument,
+        # and theoretically that could be passed through all the layers.
+
+        for d in directories:
+            self._provide_templates_from_directory(d)
 
         # We do set up the cache, though.
 
@@ -64,7 +94,8 @@ class ChameleonTemplates(TemplateSystem):
 
         logger.debug("Configuring chamelean to cache at %s", cache_dir)
         conf_mod = dottedname.resolve('chameleon.config')
-        if conf_mod.CACHE_DIRECTORY != cache_dir:  # previously imported before we set the environment
+        if conf_mod.CACHE_DIRECTORY != cache_dir:
+            # previously imported before we set the environment
             conf_mod.CACHE_DIRECTORY = cache_dir
             # Which, snarf, means the template is probably also screwed up.
             # It imports all of this stuff statically, and BaseTemplate
@@ -90,13 +121,16 @@ class ChameleonTemplates(TemplateSystem):
         # especially not if we enable macros.
         return []
 
+    get_deps = template_deps
+    get_string_deps = template_deps
+
     def render_template(self, template_name, output_name, context):
         """Renders template to a file using context.
 
         This must save the data to output_name *and* return it
         so that the caller may do additional processing.
         """
-        result = self.render_template_to_string(template_name)
+        result = self.render_template_to_string(template_name, context)
         if output_name is not None:
             makedirs(os.path.dirname(output_name))
             with open(output_name, 'wb') as f:
@@ -109,10 +143,39 @@ class ChameleonTemplates(TemplateSystem):
         # template_name is the name of the template file,
         # context is a dictionary containing the data the template
         # uses for rendering.
-        template = _PageTemplateFileWithLoad(template)
+        template = component.getMultiAdapter((self, context),
+                                             ITemplate,
+                                             name=template)
         return template(**context)
 
     def inject_directory(self, directory):
         """Injects the directory with the lowest priority in the
         template search mechanism."""
-        pass
+
+        # This is called very early, that's the only reason that it gets
+        # set low in the chains. It's called for each template plugin?
+
+        self._provide_templates_from_directory(directory)
+
+
+    def _provide_templates_from_directory(self, directory):
+        theme_zcml = os.path.join(directory, 'theme.zcml')
+        if os.path.exists(theme_zcml):
+            xmlconfig.xmlconfig(theme_zcml)
+        else:
+            # Register macros named the basename of the file
+            # for all files in the directory that end in
+            # .macro.pt
+            gsm = component.getGlobalSiteManager()
+            for macro_file in glob.glob(os.path.join(directory, "*.macro.pt")):
+                name = macro_file[:len('.macro.pt')]
+                factory = MacroFactory(macro_file, name, 'text/html')
+                gsm.registerAdapter(factory, provides=(IMacroTemplate),
+                                    requires=(interface.Interface, interface.Interface,
+                                              interface.Interface))
+
+            for template_file in glob.glob(os.path.join(directory, "*.tmpl.pt")):
+                name = template_file[:len(".tmpl.pt")]
+                factory = TemplateFactory(template_file, name, 'text/html')
+                gsm.registerAdapter(factory, provides=(ITemplate),
+                                    requires=(interface.Interface, interface.Interface))
