eaaa3c5caa44e763b7792694a68624c86b2433d2
Jason Madden <jamadden@gmail.com>
2017-10-02 10:19:07 -0500

    Initial scaffold for nti.nikola_chameleon

---
+++ README.rst
@@ -0,0 +1,27 @@
+=====================
+ nti.nikola_chameleon
+=====================
+
+.. image:: https://img.shields.io/pypi/v/nti.nikola_chameleon.svg
+        :target: https://pypi.python.org/pypi/nti.nikola_chameleon/
+        :alt: Latest release
+
+.. image:: https://img.shields.io/pypi/pyversions/nti.nikola_chameleon.svg
+        :target: https://pypi.org/project/nti.nikola_chameleon/
+        :alt: Supported Python versions
+
+.. image:: https://travis-ci.org/NextThought/nti.nikola_chameleon.svg?branch=master
+        :target: https://travis-ci.org/NextThought/nti.nikola_chameleon
+
+.. image:: https://coveralls.io/repos/github/NextThought/nti.nikola_chameleon/badge.svg?branch=master
+        :target: https://coveralls.io/github/NextThought/nti.nikola_chameleon?branch=master
+
+..
+  .. image:: https://readthedocs.org/projects/ntinikola_chameleon/badge/?version=latest
+        :target: https://ntinikola_chameleon.readthedocs.io/en/latest/
+        :alt: Documentation Status
+
+A template system for the `nikola
+<https://pypi.python.org/pypi/Nikola>`_ static blog system using
+`Chameleon <https://pypi.python.org/pypi/Chameleon>`_ and `z3c.pt
+<https://pypi.python.org/pypi/z3c.pt>`_.
---
+++ setup.py
@@ -0,0 +1,63 @@
+import codecs
+from setuptools import setup, find_packages
+
+entry_points = {
+    'console_scripts': [
+    ],
+}
+
+TESTS_REQUIRE = [
+    'nti.testing',
+    'zope.testrunner',
+]
+
+
+def _read(fname):
+    with codecs.open(fname, encoding='utf-8') as f:
+        return f.read()
+
+
+setup(
+    name='nti.nikola_chameleon',
+    version='1.0.0a1.dev0',
+    author='Jason Madden',
+    author_email='jason@nextthought.com',
+    description="NTI Nikola Chameleon",
+    long_description=(_read('README.rst') + '\n\n' + _read('CHANGES.rst')),
+    license='Apache',
+    keywords='nikola chameleon template',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    url="https://github.com/NextThought/nti.nikola_chameleon",
+    zip_safe=True,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    namespace_packages=['nti'],
+    tests_require=TESTS_REQUIRE,
+    install_requires=[
+        'setuptools',
+        'Chameleon',
+        'z3c.pt',
+        'nikola >= 7.8',
+    ],
+    extras_require={
+        'test': TESTS_REQUIRE,
+        'docs': [
+            'Sphinx',
+            'repoze.sphinx.autointerface',
+            'sphinx_rtd_theme',
+        ],
+    },
+    entry_points=entry_points,
+)
